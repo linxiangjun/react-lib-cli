94e7e3583a516449cde87fb3a9a36573c2ea7869
linxiangjun <xiangjun.lin@outlook.com>
2020-02-20 22:03:31 +0800

    优化交互设计

--- cli.js
+++ cli.js
@@ -3,6 +3,8 @@
 const program = require("commander");
 const execSync = require("child_process").execSync;
 const chalk = require("chalk");
+const inquirer = require("inquirer");
+const symbols = require("log-symbols");
 
 const library = require("./constant");
 const PKG = require("./package.json");
@@ -12,17 +14,7 @@ program.version(PKG.version);
 program
   .command("ls")
   .description("List all the popular react libraries")
-  .action(showLibrary);
-
-program
-  .command("select <index>")
-  .description("Select library index")
-  .action(onSelect);
-
-program
-  .command("install <name> <dev>")
-  .description("install selected library")
-  .action(onUse);
+  .action(onLibrary);
 
 program.parse(process.argv);
 
@@ -30,28 +22,72 @@ if (process.argv.length === 2) {
   program.outputHelp();
 }
 
-function showLibrary() {
-  let info = [];
-  Object.keys(library).forEach(function(item, index) {
-    info.push(item);
-  });
+function onLibrary() {
+  inquirer
+    .prompt([
+      {
+        name: "libraryIndex",
+        message: `请选择库类型索引\n${showLibraryType()}`
+      }
+    ])
+    .then(answers => {
+      const { libraryIndex } = answers;
+      const libraryKeys = Object.keys(library);
 
-  printMsg(info);
+      if (isNaN(libraryIndex)) {
+        return exit("输入的索引类型错误");
+      }
+
+      if (libraryIndex < 1 || libraryIndex > libraryKeys.length) {
+        return exit("输入的索引超出范围");
+      }
+
+      const libraryKey = libraryKeys[libraryIndex - 1];
+      const librarys = library[libraryKey];
+
+      inquirer
+        .prompt([
+          {
+            name: "libraryList",
+            message: `请选择库索引，多个以空格分隔(例如：1 2)\n${showLibrary(
+              librarys
+            )}`
+          },
+          {
+            name: "dev",
+            message: `请选择安装以上库到开发还是生产环境(1: 生产，2：开发)`
+          }
+        ])
+        .then(answers => {
+          const { libraryList, dev } = answers;
+          installList = "";
+
+          libraryList.split(" ").forEach(function(index) {
+            installList += `${librarys[index]} `;
+          });
+
+          onUse(installList, dev);
+        });
+    });
 }
 
-function onSelect(index) {
-  const libraryKeys = Object.keys(library);
-  if (index < 1 || index > libraryKeys.length) {
-    return exit("输入的索引不对");
-  }
-  const libraryKey = libraryKeys[index - 1];
+function showLibraryType() {
   let info = [];
-
-  Object.keys(library[libraryKey]).forEach(function(item, index) {
+  Object.keys(library).forEach(function(item) {
     info.push(item);
   });
 
-  printMsg(info);
+  return msg(info);
+}
+
+function showLibrary(librarys) {
+  let info = [];
+
+  Object.values(librarys).forEach(function(item) {
+    info.push(item);
+  });
+
+  return msg(info);
 }
 
 function onUse(name, dev) {
@@ -65,13 +101,13 @@ function exit(err) {
 }
 
 function printErr(err) {
-  console.error(chalk.red(err));
+  console.log(symbols.error, chalk.red(err));
 }
 
-function printMsg(infos) {
-  console.log("");
+function msg(infos) {
+  let msg = "";
   infos.forEach(function(info, index) {
-    console.log("  " + (index + 1) + ". " + info);
+    msg += `${index + 1}. ${info}\n`;
   });
-  console.log("");
+  return msg;
 }
--- constant.js
+++ constant.js
@@ -1,9 +1,16 @@
 const utility = {
-  lodash: "lodash"
+  1: "lodash",
+  2: "memoize-one",
+  3: "moment"
+};
+
+const style = {
+  1: "normalize"
 };
 
 const category = {
-  utility
+  utility,
+  style
 };
 
 module.exports = category;
